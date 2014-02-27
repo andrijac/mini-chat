a8649d82e3d955552056b1b5a46c8dd0b1f81985
unknown <acacanovic@gs-acacano-nb.gacjlt.local>
2014-02-27 17:14:21 +0400

    added queue

---
+++ source/MiniChat/MiniChat/scripts/queue.js
@@ -0,0 +1,19 @@
+﻿(function (window) {
+	function Queue() {
+		this.collection = [];
+	}
+
+	Queue.prototype = {
+		enqueue: function (obj) {
+			queue.push(obj);
+		},
+		dequeue: function () {
+			queue.shift();
+		},
+		getCollection: function () {
+			return this.collection;
+		}
+	}
+
+	window.Queue = Queue;
+}(window));
