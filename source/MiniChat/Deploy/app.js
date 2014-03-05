beaa919cbb1f8f42d23c2057ca5ab47e7d20018b
andrijac <andrija11@gmail.com>
2014-03-05 15:54:31 +0400

    list files

--- source/MiniChat/Deploy/app.js
+++ source/MiniChat/Deploy/app.js
@@ -3,6 +3,7 @@
 (function () {
     "use strict";
     var readline = require('readline'),
+        path = require('path'),
         fs = require('fs'),
 
         toArray = function () { return Array.prototype.slice.call(arguments[0]); },
@@ -31,17 +32,57 @@
     //==========================================================================
     //=========== FILE MERGING =================================================
     //==========================================================================
-    (function(){
-        var filename = "C:/Users/acacanovic/Documents/STORE/GIT_ROOT/mini-chat/source/MiniChat/MiniChat/client.js";
-        //process.argv[0];
-        var readAllText = function (filename, callback) {            
-            fs.readFile(filename, 'utf8', function(err, data) {
-                if (err) throw err;
-                callback(data);
-            });            
-        };
+    (function () {
 
-        readAllText(filename, function(e){ wl(e); });
+        function findBlock(text, startTag, stopTag) {
+            var lines = text.split("\n"), i, ii, start, line, result = [];
+
+            start = false;
+
+            for (i = 0, ii = lines.length; i < ii; i += 1) {
+                line = lines[i].trim();
+
+                if (line === startTag) {
+                    start = true;
+                    continue;
+                }
+
+                if (!start) {
+                    continue;
+                }
+
+
+                if (line === stopTag) {
+                    break;
+                }
+
+                result.push(line);
+                
+            }
+
+            return result;
+        }
+
+        var filename = "C:/Users/acacanovic/Documents/STORE/GIT_ROOT/mini-chat/source/MiniChat/MiniChat/client.html",
+        //process.argv[0];
+            readAllText = function (filename, callback) {
+                fs.readFile(filename, 'utf8', function (err, data) {
+                    if (err) {
+                        wl(err);
+                        return;
+                    }
+
+                    callback(filename, data);
+                });
+            };
+
+        readAllText(filename, function (filename, e) {
+           
+            var files = findBlock(e, "<!-- scripts", "end -->");
+
+            files.forEach(function(e) { log(e); });
+
+        });
 
     }());
 
