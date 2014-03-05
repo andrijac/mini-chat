31dca86e79b8eb920fe2c0fac7326d73343d9170
andrijac <andrija11@gmail.com>
2014-03-05 11:27:18 +0400

    /nick command

--- source/MiniChat/MiniChat/client.js
+++ source/MiniChat/MiniChat/client.js
@@ -9,6 +9,8 @@
 		var username,
 			room,
 			lastMessage = null,
+			commands,
+			listener,
 
 			// DOM elements
 			output = h.getEl("output"),
@@ -16,7 +18,8 @@
 			btnEnter = h.getEl("btnEnter"),
 
 			// message output html template
-			messageTemplate = '<div><b style="margin-right: 3px;">{{UserName}}:</b><span id="{{ID}}">{{MessageValue}}</span></div>',
+			messageTemplate = '<div><b style="margin-right: 3px;">{{UserName}}:</b><span id="{{ID}}" class="messageBody">{{MessageValue}}</span></div>',
+			sysMessageTemplate = '<div><span class="sysMessageBody">{{MessageValue}}</span></div>',
 
 			// Constants
 			requestType = {
@@ -25,17 +28,18 @@
 			},
 
 			// Configuration
-			serverUrl = "ChatServer.ashx",
-
-			listener = new h.Listener(1000, function () {
-				sendRequest({
-					"LastMessage": lastMessage
-				},
-				requestType.listener,
-				callback);
-			});
+			serverUrl = "ChatServer.ashx";
 
 		//============================================
+
+		function Cmd(c, h) {
+			this.cmd = c;
+			this.handler = h;
+		}
+
+		commands = [
+			new Cmd("nick", nickCommand)
+		];
 
 		//function ExecInfo() {
 		//	this.url = "";
@@ -48,11 +52,13 @@
 		(function Main() {
 			//username = prompt("Choose your nickname:", "");
 
-			username = 'test';
+			username = 'user';
 
 			if (!username) {
 				return;
 			}
+
+			listener = new h.Listener(1000, listenerHandler);
 
 			// read room from query string
 			room = h.readQuery("?room");
@@ -84,14 +90,35 @@
 
 		// Functions
 		function sendMessage() {
-			var requestData = {
-				"UserName": username,
-				"MessageValue": input.value
-			};
+			var inputVal = input.value.trim(),
+				commandFind,
+				requestData;
+
+			if (!inputVal) return;
+
+			commandFind = getCommand(inputVal)
+
+			if (commandFind) {
+				commandFind.cmd.handler(commandFind.value);
+			}
+			else {
+				requestData = {
+					"UserName": username,
+					"MessageValue": inputVal
+				};
+
+				sendRequest(requestData, requestType.sendMessage, callback);
+			}
 
 			input.value = "";
+		}
 
-			sendRequest(requestData, requestType.sendMessage, callback);
+		function listenerHandler() {
+			sendRequest({
+				"LastMessage": lastMessage
+			},
+			requestType.listener,
+			callback);
 		}
 
 		function sendRequest(data, commandParam, callback) {
@@ -120,7 +147,7 @@
 			}
 
 			for (i = 0; i < e.MessageList.length; i++) {
-				addMessage(e.MessageList[i]);
+				addMessage(e.MessageList[i], messageTemplate);
 			}
 
 			message = e.MessageList[e.MessageList.length - 1];
@@ -131,10 +158,52 @@
 		}
 
 		// add message to output container
-		function addMessage(message) {
-			var result = h.executeTemplate(message, messageTemplate);
+		function addMessage(message, template) {
+			var result = h.executeTemplate(message, template);
 
 			output.innerHTML += result;
+		}
+
+		function getCommand(val) {
+			if (!val
+				|| val[0] != "/") return;
+
+			var input = val.trim(),
+				split = input.split(" "),
+				command = split[0].substring(1),
+				value = split[1].substring(0),
+				returnCmd,
+				returnValue;
+
+			commands.forEach(function (e, i, array) {
+				if (e.cmd == command) {
+					returnCmd = e;
+					returnValue = value;
+
+					return true;
+				}
+
+				return false;
+			})
+
+			return {
+				cmd: returnCmd,
+				value: returnValue
+			};
+		}
+
+		//============================================
+		//=========== COMMANDS =======================
+		//============================================
+
+		function nickCommand(val) {
+			if (!val) return;
+
+			username = val.trim().split(" ")[0].trim();
+
+			addMessage({
+				"MessageValue": "username changed to '" + username + "'"
+			}, sysMessageTemplate);
 		}
 	}(window, document));
 });
