be5b0c639afafbbbc6b66e16b87b1f69996fe47f
andrijac <andrija11@gmail.com>
2014-03-05 14:58:34 +0400

    jslint warnings

--- source/MiniChat/Deploy/app.js
+++ source/MiniChat/Deploy/app.js
@@ -1,27 +1,32 @@
-var readline = require('readline');
+/*global process, require, console, arguments*/
 
-var toArray = function(){return Array.prototype.slice.call(arguments[0]);}
-var log =   function() {
-                var arr = toArray(arguments).map(function(v) {
-                   // debugger;
-                    return v + ''; 
+(function () {
+    "use strict";
+    var readline = require('readline'),
+        fs = require('fs'),
+
+        toArray = function () { return Array.prototype.slice.call(arguments[0]); },
+        log =   function () {
+            var arr = toArray(arguments).map(function (v) {
+                    // debugger;
+                    return v.toString();
                 });
-                console.log(arr.join(" "));
-            }
-var blank = function(){log("");}
-var wl = function(){log.apply(this, toArray(arguments)); blank();}
+            console.log(arr.join(" "));
+        },
+        blank = function () { log(""); },
+        wl = function () { log.apply(this, toArray(arguments)); blank(); },
+        rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-blank();
+    blank();
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+    if (process.argv.length < 3) {
+        process.argv.forEach(function (e) {
+            wl(e);
+        });
+    }
 
-if (process.argv.length < 3) {
-	process.argv.forEach(function (e) {        
-		wl(e);
-	});	
-}
-
-rl.question("", function(){rl.close();});
+    rl.question("", function () { rl.close(); });
+}());
